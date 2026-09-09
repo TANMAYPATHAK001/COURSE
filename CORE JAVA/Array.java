e263e958b8cca1cc9a58667965fc7d9218599e49
TANMAYPATHAK001 <tanmay239kr@gmail.com>
2026-09-09 12:53:24 +0530

    Enhanced for loop

--- CORE JAVA/Array.java
+++ CORE JAVA/Array.java
@@ -31,8 +31,14 @@ public class Array {
     students[1] =  s2;
     students[2] =  s3;
 
-    for(int i=0; i<students.length; i++){
-        System.out.println(students[i]. name + " : " + students[i].marks);
+    // for(int i=0; i<students.length; i++){
+    //     System.out.println(students[i]. name + " : " + students[i].marks);
+    // }
+
+    // OR
+
+    for(Student stud : students){
+        System.out.println(stud.name + " : " + stud.marks);
     }
 
     int nums[] = new int[4];
---
+++ CORE JAVA/ForEach.java
@@ -0,0 +1,20 @@
+public class ForEach {
+    public static void main(String[] args) {
+        
+        int nums[] = new int[4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 3;
+        nums[3] = 9;
+    
+        // for(int i=0; i<4; i++){
+        //     System.out.println(nums[i]);
+        // }
+
+        // FOR EACH
+        for(int n : nums){
+            System.out.println(n);
+        }
+
+    }
+}
